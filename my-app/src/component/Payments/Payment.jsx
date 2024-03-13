1d6725def284f51316b9cf16dff3d6e8725bddac
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-14 00:00:43 +0530

    Payment component is ready

--- my-app/src/component/Payments/Payment.jsx
+++ my-app/src/component/Payments/Payment.jsx
@@ -1,45 +1,70 @@
-    import React from "react";
-    import "./Payment.css";
-    import { FaArrowRight } from "react-icons/fa6";
-    function Payment() {
-    return (
-        <div>
-        <div className="payment-cart">
-            <div className="pay-title">
-            <h1>Payments</h1>
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { FaCircle } from "react-icons/fa";
+import "./Payment.css";
+function Payment() {
+  return (
+    <>
+      <div className="payment-card">
+        <h1>Payments</h1>
+        <div className="one">
+          <div className="payment-header">
+            <div className="transaction">
+              <h5>Transaction</h5>
             </div>
-            <div className="payment-heading">
-            <div className="transaction">Transaction</div>
-            <div className="amount">Amount</div>
+            <div className="amount">
+              <h5>Amount</h5>
             </div>
-            <div className="products">
-            <div className="item">
-                <p>Consulation with doctor</p>
-                <FaArrowRight color="blue" />
-                <h6>20$</h6>
-            </div>
-            <div className="item">
-                <p>Medicine </p>
-                <FaArrowRight color="blue" />
-                <h6>20$</h6>
-            </div>{" "}
-            <div className="item">
-                <p>Consulation with doctor</p>
-                <FaArrowRight color="blue" />
-                <h6>20$</h6>
-            </div>
-            </div>
-            <div className="bill">
-                <div>
-                    Total amount
-                </div>
-                <div>
-                    40$
-                </div>
-            </div>
+          </div>
         </div>
+        <div className="two">
+          <div className="transaction-details">
+            <div className="transaction-name">
+              <div className="name">
+                <FaCircle color="green" size={"15px"} />
+                Consultation with doctor
+              </div>
+              <div className="arrow">
+                <FaArrowRight size={"1.3rem"} color={"royalblue"} />
+              </div>
+            </div>
+            <div className="transaction-price">20 $</div>
+            
+          </div>
+          <div className="transaction-details">
+            <div className="transaction-name">
+              <div className="name">
+                <FaCircle color="green" size={"15px"} />
+                Medicine
+              </div>
+              <div className="arrow">
+                <FaArrowRight  size={"1.3rem"}  color={"royalblue"} />
+              </div>
+            </div>
+            <div className="transaction-price">50 $</div>
+          </div>
+          <div className="transaction-details">
+            <div className="transaction-name">
+              <div className="name">
+                <FaCircle color="green" size={"15px"} />
+                Consultation with doctor
+              </div>
+              <div className="arrow">
+                <FaArrowRight  size={"1.3rem"} color={"royalblue"} />
+              </div>
+            </div>
+            <div className="transaction-price">20 $</div>
+          </div>
         </div>
-    );
-    }
+        <div className="three">
+          <div className="payment-bottom">
+            <div className="total-amount"><h5>Total Amount</h5></div>
+            <div className="total"><h5>90 $</h5></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
-    export default Payment;
+export default Payment;
